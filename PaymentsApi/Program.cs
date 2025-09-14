813c45e54081d94a84344bbceb667f7862280281
Natasja K <natasja.kauppi@gmail.com>
2025-09-14 15:49:30 +0200

    azure

--- PaymentsApi/Program.cs
+++ PaymentsApi/Program.cs
@@ -12,7 +12,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment()) { app.UseSwagger(); app.UseSwaggerUI(); }
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/", () => Results.Redirect("/swagger"));
+
+app.MapGet("/health", () => Results.Ok("OK"));
 
 app.MapControllers();
 
